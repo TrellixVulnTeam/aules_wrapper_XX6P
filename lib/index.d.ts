41a9300243ee9ecdbc09afdef83aa3bb316bb4c2
danisty <danisty.nori@gmail.com>
2022-02-11 22:41:14 +0100

    Fixed dependencies problems

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,26 +1,18 @@
 import { AxiosInstance } from 'axios';
-import { HTMLElement } from 'node-html-parser';
-interface User {
-    id: string | undefined;
-    name: string | undefined;
-    email: string | undefined;
-    profilePicture: string | undefined;
-}
-interface QuickEvent {
-    icon: string;
-    title: string;
-    time: string;
-}
+import { User, Event, Class } from './interfaces';
+declare const _default: undefined;
+export default _default;
 export declare const Sessions: Session[];
 export declare class Session {
     private jar;
     private baseURL;
+    private sesskey;
     request: AxiosInstance;
     constructor(cookie?: string);
     private apiRequest;
     login(course: string, username: string, password: string): Promise<any>;
     isSessionValid(): Promise<boolean>;
     getUserInfo(): Promise<User>;
-    getEvents(personalAreaDoc?: HTMLElement): Promise<QuickEvent[]>;
+    getEvents(): Promise<Event[]>;
+    getClasses(): Promise<Class[]>;
 }
-export {};
---
+++ lib/interfaces.d.ts
@@ -0,0 +1,66 @@
+declare const _default: undefined;
+export default _default;
+export interface User {
+    id: string;
+    name: string;
+    email: string;
+    profilePicture: string;
+}
+export interface Event {
+    id: number;
+    name: string;
+    description: string;
+    modulename: string;
+    instance: number;
+    eventtype: string;
+    timestart: number;
+    timeduration: number;
+    timesort: number;
+    visible: number;
+    timemodified: number;
+    icon: Icon;
+    course: Class;
+    formattedtime: string;
+    isactionevent: boolean;
+    iscourseevent: boolean;
+    iscategoryevent: boolean;
+    groupname: object;
+    normalisedeventtype: string;
+    normalisedeventtypetext: string;
+    action: Action;
+    url: string;
+    islastday: boolean;
+    popupname: string;
+}
+export interface Class {
+    id: number;
+    fullname: string;
+    shortname: string;
+    idnumber: string;
+    summary: string;
+    summaryformat: number;
+    startdate: number;
+    enddate: number;
+    visible: boolean;
+    fullnamedisplay: string;
+    viewurl: string;
+    courseimage: string;
+    progress: number;
+    hasprogress: boolean;
+    isfavourite: boolean;
+    hidden: boolean;
+    showshortname: boolean;
+    coursecategory: string;
+}
+export interface Icon {
+    key: string;
+    component: string;
+    alttext: string;
+}
+export interface Action {
+    name: string;
+    url: string;
+    itemcount: number;
+    actionable: boolean;
+    showitemcount: boolean;
+}
