7c6732d67ccf12a29e4cea49e33ecb95c72f18d2
danisty <danisty.nori@gmail.com>
2022-02-14 17:33:16 +0100

    Uploaded latest build

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,14 +1,26 @@
 import { AxiosInstance } from 'axios';
 import { User, Event, Class, SessionOptions } from './definitions';
+interface Cookie {
+    key: string;
+    value: string;
+}
+declare class CookieJar {
+    cookies: Cookie[];
+    constructor();
+    addCookie: (key: string, value: string) => void;
+    removeCookie: (key: string) => Cookie[];
+    addCookiesFromHeaders: (headers: any) => any;
+    toString: () => string;
+}
 declare const _default: undefined;
 export default _default;
 export declare const Sessions: Session[];
 export declare class Session {
-    private jar;
     private baseURL;
     private sesskey;
     private options;
     request: AxiosInstance;
+    cookieJar: CookieJar;
     constructor(options?: SessionOptions);
     private apiRequest;
     login(course?: string, username?: string, password?: string): Promise<any>;
